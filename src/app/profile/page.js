dad9eed227b6e2ccd051f1db8cbc43eb9889d680
mahirasef09 <mahir.asef09@gmail.com>
2025-01-24 14:13:59 +0600

    Adding middleware

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,16 +1,7 @@
 
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
-
-
 export default async function Profile() {
     
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-
-    if (!user) {
-        redirect("/api/auth/login");
-    }
+    
 
     return (
         <div>
