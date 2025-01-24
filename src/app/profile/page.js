d1097a9cca315c2438655befbd8416e7d2423bd8
mahirasef09 <mahir.asef09@gmail.com>
2025-01-24 12:16:31 +0600

    Profile section updated

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,21 +4,13 @@ import { redirect } from "next/navigation";
 
 
 export default async function Profile() {
-    const { isAuthenticated } = getKindeServerSession();
+    
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-    if (!(await isAuthenticated())) {
+    if (!user) {
         redirect("/api/auth/login");
     }
-
-
-    // return (await isAuthenticated()) ? (
-    //     <div>
-    //         <h2 className="text-center text-5xl font-bold my-5">Welcome to your profile!</h2>
-    //     </div>):
-    // (<div>
-    //     This page is protected, please <LoginLink>Login</LoginLink> to view it
-    // </div>
-    // );
 
     return (
         <div>
